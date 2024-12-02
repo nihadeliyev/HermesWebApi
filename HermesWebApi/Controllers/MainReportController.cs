8690e6338a7a618c999591c6a0010142ab17e725
Nihad Aliyev <nihad.eliyev@gmail.com>
2024-12-02 16:59:31 +0400

    filtirsiz basliq hisse

---
+++ HermesWebApi/Controllers/MainReportController.cs
@@ -0,0 +1,66 @@
+﻿using HermesWebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HermesWebApi.Controllers
+{
+    public class MainReportController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
+
+        SqlConnection gCon;
+        public MainReportController(IUserService userService, IConfiguration configuration)
+        {
+            _userService = userService;
+            _configuration = configuration;
+            gCon = new SqlConnection(_configuration["ConnectionStrings:Default"]);
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpGet("All"), Authorize]
+        public IActionResult Get(int? companyID, int? categoryID, int? trainingID, DateTime? fromDate, DateTime? toDate)
+        {
+            var userID = _userService.GetUserId();
+
+            if (userID == null)
+                return Unauthorized("Unable to find user information.");
+
+            string sql = @"
+SELECT CASE WHEN P.Date_<=getdate() THEN 1 ELSE 0 end AS Done, C.CategoryID, C.CategoryName,  COUNT(P.ID) PlanCount, SUM(PH.TotalDays) TotalDays, SUM(PH.TotalHours) TotalHours  FROM TRPlannedTrainings P 
+LEFT JOIN MDTrainings T ON P.TrainingID=T.TrainingID
+LEFT JOIN MDTrainingCategories C ON T.Category=C.CategoryID
+LEFT JOIN (select P.ID,COUNT(D.ID) TotalDays,  COUNT(D.ID)* convert(float, DATEDIFF(MINUTE, StartTime,EndTime))/60 TotalHours from TRPlannedTrainings P
+			LEFT JOIN TRPlanDates D ON P.ID=D.PlanID GROUP BY P.ID,StartTime,EndTime) PH on P.ID=PH.ID
+
+group by CASE WHEN P.Date_<=getdate() THEN 1 ELSE 0 end, C.CategoryID, C.CategoryName";
+            DataSet ds = new DataSet();
+            ResultCode res = Db.GetDbDataWithConnection(ref gCon, sql, ref ds, new SqlParameter("UserID", userID));
+            if (res != ResultCodes.noError)
+                return NotFound("Data could not be found");
+            MainReport report = new MainReport();
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                report.CategoryStatistics.Add(new CategoryStatistic()
+                {
+                    Category = new TrainingCategory()
+                    {
+                        ID = ds.Tables[0].Rows[i]["CategoryID"].ToString(),
+                        Name = ds.Tables[0].Rows[i]["CategoryName"].ToString()
+                    },
+                    TrainingCount = int.Parse(ds.Tables[0].Rows[i]["PlanCount"].ToString()),
+                    TotalDays = int.Parse(ds.Tables[0].Rows[i]["TotalDays"].ToString()),
+                    TotalHours = int.Parse(ds.Tables[0].Rows[i]["TotalHours"].ToString()),
+                    Done = ds.Tables[0].Rows[i]["Done"].ToString() == "1"
+                });
+            }
+            report.CompletedTrainingCount = report.CategoryStatistics.Where(x => x.Done == true).Sum(k => k.TrainingCount);
+            report.UpcomingTraining = report.CategoryStatistics.Where(x => x.Done == false).Sum(k => k.TrainingCount);
+            report.CompletedTrainingHours = report.CategoryStatistics.Where(x => x.Done == true).Sum(k => k.TotalHours);
+            report.UpcomingTrainingHours = report.CategoryStatistics.Where(x => x.Done == false).Sum(k => k.TotalHours);
+            return Ok(report);
+        }
+    }
+}
---
+++ HermesWebApi/Models/MainReport.cs
@@ -0,0 +1,40 @@
+﻿namespace HermesWebApi.Models
+{
+    public class MainReport
+    {
+        public List<CategoryStatistic> CategoryStatistics { get; set; }
+        public int CompletedTrainingCount { get; set; }
+        public int UpcomingTraining { get; set; }
+        public int TotalTrainingCount
+        {
+            get
+            {
+                return UpcomingTraining + CompletedTrainingCount;
+            }
+        }
+        public int CompletedTrainingHours { get; set; }
+        public int UpcomingTrainingHours { get; set; }
+        public int TotalTrainingHours
+        {
+            get
+            {
+                return CompletedTrainingHours + UpcomingTrainingHours;
+            }
+        }
+
+        public MainReport()
+        {
+            CategoryStatistics=new List<CategoryStatistic>();
+        }
+
+    }
+    public class CategoryStatistic
+    {
+        public TrainingCategory Category { get; set; }
+        public int TrainingCount { get; set; }
+        public bool Done { get; set; }
+        public int TotalDays { get; set; }
+        public int TotalHours { get; set; }
+
+    }
+}
